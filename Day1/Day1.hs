2f185127b1ba1026f686aae734c6479b831324cc
Alexander Rogers <awr90@outlook.com>
2025-10-08 20:59:47 +0100

    Day 1  part 2

--- Day1/Day1.hs
+++ Day1/Day1.hs
@@ -1,11 +1,16 @@
 module Main where
 import Data.List
 import Data.Bifunctor
+import qualified Data.IntMap.Strict as IntMap
+import Data.IntMap (IntMap)
 
 main::IO()
 main = do
     fileData <- readFile "./day1inp.txt"
-    print (getTotalDiff (sortTup (interpritFile fileData)))
+    let lPair = interpritFile fileData
+    print (getTotalDiff (sortTup lPair))
+    print (totalSimScore (fst lPair) (getCounts (snd lPair)))
+
 
 stripFileTokens :: String -> [[String]]
 stripFileTokens contents = map words (lines contents)
@@ -24,3 +29,26 @@ sortTup = bimap sort sort
 
 getTotalDiff :: ([Int], [Int]) -> Int
 getTotalDiff tup = sum (map getDiff2 (uncurry zip tup))
+
+updateIntMap :: Int -> IntMap Int -> IntMap Int
+updateIntMap key = IntMap.insertWithKey f key 1 
+    where
+         f :: Int -> Int -> Int -> Int
+         f key nVal oVal = oVal + 1
+
+getCounts :: [Int] -> IntMap Int
+getCounts = go IntMap.empty
+    where
+        go :: IntMap Int -> [Int] -> IntMap Int
+        go pMap l = 
+            if null l
+            then pMap
+            else 
+                let val = head l
+                in go (updateIntMap val pMap) (tail l)
+
+simScore :: IntMap Int -> Int -> Int
+simScore lMap val = val * IntMap.findWithDefault 0 val lMap
+
+totalSimScore :: [Int] -> IntMap Int -> Int
+totalSimScore l lMap = sum (map (simScore lMap) l)
