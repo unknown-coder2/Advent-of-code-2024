579f763e4295efa43fb92ad09612b7e656535010
Alexander Rogers <awr90@outlook.com>
2025-10-15 19:33:51 +0100

    Day 2  Part 2

--- Day2/Day2.hs
+++ Day2/Day2.hs
@@ -4,7 +4,7 @@ main::IO()
 main = do
     fileData <- readFile "./day2inp.txt"
     print (checkValidTotal (interpritFile fileData))
-    -- print (minimum (map length (interpritFile fileData)))
+    print (remove [0, 1, 2, 3, 4, 5, 6, 7, 8, 9] 9)
 
 interpritFile :: String -> [[Int]]
 interpritFile cont =
@@ -12,7 +12,7 @@ interpritFile cont =
     in map (map read) inter
 
 checkValidTotal :: [[Int]] -> Int
-checkValidTotal records = sum (map checkValid records)
+checkValidTotal records = sum (map checkValidWithRemoval records)
 
 checkValid :: [Int] -> Int
 checkValid level = go level 1 (getDirection level)
@@ -28,6 +28,27 @@ checkValidPair :: [Int] -> Int -> Bool
 checkValidPair l upOrDown =
     let dif = (l !! 1) - head l
     in (dif * upOrDown) > 3 || (dif * upOrDown) < 1
+
+checkValidWithRemoval :: [Int] -> Int
+checkValidWithRemoval level = go level 1 (getDirection level)
+    where
+        go :: [Int] -> Int -> Int -> Int
+        go l valid upOrDown
+          | valid == 0 = 0
+          | null (drop 1 l) = valid
+          | checkValidPair l upOrDown = tryRemoval l level
+          | otherwise = go (tail l) 1 upOrDown
+
+tryRemoval :: [Int] -> [Int] -> Int
+tryRemoval l level
+  | checkValid (remove level (length level - length l)) == 1 = 1
+  | checkValid (remove level (length level - length l + 1)) == 1 = 1
+  | otherwise = 0
+
+
+remove :: [Int] -> Int -> [Int]
+remove l idx =
+    take idx l ++ drop (idx + 1) l
 
 getDirection :: [Int] -> Int
 getDirection record =
